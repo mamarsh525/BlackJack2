04acfe92c1d2054eab2c805e71cd6ec1266ae9d4
Justin <justin.andreoli04@gmail.com>
2025-05-08 16:24:18 -0700

    The menu start button now sends the user to the GameActivity

--- app/src/main/java/com/example/myapplication/MenuActivity.java
+++ app/src/main/java/com/example/myapplication/MenuActivity.java
@@ -1,12 +1,11 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -15,12 +14,15 @@ public class MenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button startGame = findViewById(R.id.startGameButton);
+        startGame.setOnClickListener(v -> {
+            Intent intent = new Intent(MenuActivity.this, GameActivity.class);
+            startActivity(intent);
         });
+
     }
+
 
 
 }
